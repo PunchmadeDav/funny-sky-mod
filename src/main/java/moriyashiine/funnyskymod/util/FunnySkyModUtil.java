63a58c0fbad980993c517e25cb59ee53f0c37043
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2022-11-16 08:11:37 -0600

    remove unnecessary call

--- src/main/java/moriyashiine/funnyskymod/util/FunnySkyModUtil.java
+++ src/main/java/moriyashiine/funnyskymod/util/FunnySkyModUtil.java
@@ -3,14 +3,16 @@ package moriyashiine.funnyskymod.util;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
 public class FunnySkyModUtil {
 	public static double getMultiplier() {
-		if (MinecraftClient.getInstance().getCameraEntity() == null) {
+		Entity cameraEntity = MinecraftClient.getInstance().getCameraEntity();
+		if (cameraEntity == null) {
 			return 1;
 		}
-		return MathHelper.clamp((300 - MinecraftClient.getInstance().getCameraEntity().getY()) / 10, 0, 1);
+		return MathHelper.clamp((300 - cameraEntity.getY()) / 10, 0, 1);
 	}
 }
